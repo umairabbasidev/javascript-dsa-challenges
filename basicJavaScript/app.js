b219e05273dbb23e911c7ccf18ee01311f474ef0
muhammad umair <muhammadumairabbasi637@gmail.com>
2025-01-03 20:30:46 +0500

    first 10 dsa challenges completed.date:3/1/25

---
+++ basicJavaScript/app.js
@@ -0,0 +1,79 @@
+//  Comment Your JavaScript Code
+// 1 Try creating one of each type of comment.
+
+// This is an in-line comment.
+/*
+This is a multi-line comment
+*/
+
+// Declare JavaScript Variables
+// 2 Use the var keyword to create a variable called myName.
+
+// var myName;
+
+// Storing Values with the Assignment Operator
+// 3 Assign the value 7 to variable a.
+
+// var a;
+// a = 7;
+
+// Assigning the Value of One Variable to Another
+// 4 Assign the contents of a to variable b.
+
+// var a;
+// a = 7;
+// var b;
+// b = a;
+// console.log(a,b);
+
+// Initializing Variables with the Assignment Operator
+// 5 Initializing Variables with the Assignment Operator
+
+// var a = 9;
+
+// Declare String Variables
+// 6 Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+// var myFirstName = "Muhammad Umair";
+// var myLastName = "Abbasi"
+
+// Understanding Uninitialized Variables
+// 7 Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+
+// var a = 5;
+// var b = 10;
+// var c = "I am a";
+// console.log(a); // undefined
+// a = a + 1;  // a++
+// b = b + 5; // a += 5
+// c = c + " String!";
+// console.table([a, b, c]);
+
+// Understanding Case Sensitivity in Variables
+// 8 Modify the existing declarations and assignments so their names use camelCase
+
+// Variable declarations
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+
+// Variable assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+
+// Explore Differences Between the var and let Keywords
+// 9 Update the code so it only uses the let keyword.
+
+// let catName = "Oliver";
+// let catSound = "Meow!"
+
+// Declare a Read-Only Variable with the const Keyword
+// 10 Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. Also, rename variables declared with const to conform to common practices. Do not change the strings assigned to the variables.
+
+// const FCC = "freeCodeCamp"; 
+// let fact = "is cool!"; 
+// fact = "is awesome!";
+// console.log(FCC, fact); 
+
+// Today 10 DSA challenges completed. Date: 03/01/2025`
---
+++ readme.md
@@ -0,0 +1,43 @@
+# 🚀 DSA Challenges Journey
+
+Welcome to my **Data Structures and Algorithms (DSA)** repository! This is my space to improve problem-solving skills and master core concepts.  
+
+---
+
+## 📂 Repository Structure
+- **Basic JavaScript**
+- **ES6 Features**
+- **Regular Expressions**
+- **Debugging**
+- **Basic Data Structures**
+- **Algorithm Scripting**
+
+---
+
+## 🏆 Challenges Overview
+
+### 🟢 Basic JavaScript
+- **[Comment Your JavaScript Code](./basic-javascript/comment-your-javascript-code.js)**  
+- **[Declare JavaScript Variables](./basic-javascript/declare-javascript-variables.js)**  
+
+### 🟠 ES6 Features
+- **[Arrow Functions](./es6/arrow-functions.js)**  
+- **[Destructuring Assignment](./es6/destructuring-assignment.js)**  
+
+### 🟣 Regular Expressions
+- **[Match Patterns](./regex/match-patterns.js)**  
+
+### 🔵 Algorithm Scripting
+- **[Convert Celsius to Fahrenheit](./algorithm-scripting/convert-celsius-to-fahrenheit.js)**  
+- **[Reverse a String](./algorithm-scripting/reverse-a-string.js)**  
+
+---
+
+## 🎯 Goals
+1. Master DSA concepts.
+2. Solve real-world problems.
+3. Build competitive programming skills.
+
+---
+
+⭐ **Star this repository to follow my progress!**
