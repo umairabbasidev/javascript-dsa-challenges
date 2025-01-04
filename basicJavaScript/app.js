fe5cca26d145e5bc1b6b65760db16b884fb81543
muhammad umair <muhammadumairabbasi637@gmail.com>
2025-01-04 11:33:44 +0500

    todays 37 dsa challenges completd

--- basicJavaScript/app.js
+++ basicJavaScript/app.js
@@ -71,9 +71,276 @@ This is a multi-line comment
 // Declare a Read-Only Variable with the const Keyword
 // 10 Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. Also, rename variables declared with const to conform to common practices. Do not change the strings assigned to the variables.
 
-// const FCC = "freeCodeCamp"; 
-// let fact = "is cool!"; 
+// const FCC = "freeCodeCamp";
+// let fact = "is cool!";
 // fact = "is awesome!";
-// console.log(FCC, fact); 
+// console.log(FCC, fact);
 
 // Today 10 DSA challenges completed. Date: 03/01/2025`
+
+// Add Two Numbers with JavaScript
+// 11 Change the 0 so that sum will equal 20.
+
+// const sum = 10 + 10;
+
+// Subtract One Number from Another with JavaScript
+// 12 Change the 0 so the difference is 12.
+
+// const difference = 45 - 33;
+
+// Multiply Two Numbers with JavaScript
+// 13 Change the 0 so that product will equal 80.
+
+// const product = 8 * 10;
+
+// Divide One Number by Another with JavaScript
+// 14 Change the 0 so that the quotient is equal to 2.
+
+// const quoient = 66 / 33;
+
+// Increment a Number with JavaScript
+// 15 Change the code to use the ++ operator on myVar.
+
+// let myVar = 87;
+// 16 myVar++; // long syntex myVar = myVar + 1;or compund assignment with augmented addition myVar += 1;
+// console.log(myVar);
+
+// Decrement a Number with JavaScript
+// 17 Change the code to use the -- operator on myVar.
+
+// let myVar = 11;
+// myVar--; // myVar = myVar - 1; or myVar -= 1;
+
+// Create Decimal Numbers with JavaScript
+// 18 Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
+
+// const myDecimal = 5.7;
+
+// Multiply Two Decimals with JavaScript
+// 19 Change the 0.0 so that product will equal 5.0.
+
+// const product = 2.0 * 2.5;
+
+// Divide One Decimal by Another with JavaScript
+// 20 Change the 0.0 so that quotient will equal to 2.2.
+
+// const quotient = 4.4 / 2.0;
+
+// Finding a Remainder in JavaScript
+// 21 Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operato
+// const remainder = 11 % 3; // 11 / 3 = 3(quotient), 2(remainder)
+// console.log(11 / 3); // 3
+// console.log(remainder);
+
+// Compound Assignment With Augmented Addition
+// 22 Convert the assignments for a, b, and c to use the += operator.
+
+// let a = 3;
+// let b = 17;
+// let c = 12;
+
+// assignments using compound assignment with augmented addition;
+
+// a += 12; // a = a + 12;
+// b += 9; // b = b + 9;
+// c += 7 // c = c + 7;
+
+// Compound Assignment With Augmented Subtraction
+// 23 Convert the assignments for a, b, and c to use the -= operator.
+
+// let a = 11;
+// let b = 9;
+// let c = 3;
+
+// a -= 6;
+// b -= 15;
+// c -= 1;
+
+//  Compound Assignment With Augmented Multiplication
+// 24 Convert the assignments for a, b, and c to use the *= operator.
+
+// let a = 5;
+// let b = 12;
+// let c = 4.6;
+
+// a *= 5;
+// b *= 3;
+// c *= 10;
+
+// Compound Assignment With Augmented Division
+// 25 Convert the assignments for a, b, and c to use the /= operator.
+
+// let a = 48;
+// let b = 108;
+// let  c = 33;
+
+// a /= 12;
+// b /= 4;
+// c /= 11;
+
+// Escaping Literal Quotes in Strings
+// 26 Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+
+// const myStr = "I am a \"double quoted\" string inside \"double quotes\""
+// console.log(myStr);
+
+// Quoting Strings with Single Quotes
+// 27 Change the provided string to a string with single quotes at the beginning and end and no escape characters.
+
+// Right now, the <a> tag in the string uses double quotes everywhere. You will need to change the outer quotes to single quotes so you can remove the escape characters.
+
+// const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// Escape Sequences in Strings
+// 28 Assign the following three lines of text into the single variable myStr using escape sequences.
+
+// FirstLine
+//  \SecondLine
+// ThirdLine
+
+// const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"
+
+// Concatenating Strings with Plus Operator
+// 29 Build myStr from the strings This is the start. and This is the end. using the + operator. Be sure to include a space between the two strings.
+
+// const myStr = "This is the start. " + "This is the end."
+
+// Concatenating Strings with the Plus Equals Operator
+// 30 Build myStr over several lines by concatenating these two strings: This is the first sentence. and This is the second sentence. using the += operator. Use the += operator
+
+// let myStr = "This is the first sentence. ";
+// myStr  += "This is the second sentence."
+// console.log(myStr);
+
+// Constructing Strings with Variables
+// 31 Set myName to a string equal to your name and build myStr with myName between the strings My name is  and  and I am well!
+
+// const myName = "Muhammad Umair absasi";
+// const myStr = "My name is " + myName + " and I am well!";
+
+// Appending Variables to Strings
+// 32 Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+
+// const someAdjective = "Excellent";
+// let myStr = "Learnig to code is ";
+// myStr += someAdjective;
+// console.log(myStr);
+
+// Find the Length of a String
+// 33 Use the .length property to set lastNameLength to the number of characters in lastName.
+
+// let lastNameLength = 0;
+// const lastName = "Lovelace";
+// lastNameLength = lastName.length;
+
+// Use Bracket Notation to Find the First Character in a String
+// 34 Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+
+// let firstLetterOfLastName = "";
+// const lastName = "Lovelace";
+// firstLetterOfLastName = lastName[0];
+
+// Understand String Immutability
+// 35 Correct the assignment to myStr so it contains the string value of Hello World using the approach shown in the example above.
+
+// let myStr = "Jello World";
+// myStr[0] = "H"; // noow possible bcz string is immutable
+// myStr = "Hello World";
+
+// Use Bracket Notation to Find the Nth Character in a String
+// 36 Let's try to set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
+
+// const lastName = "Lovelace";
+// const thirdLetterOfLastName = lastName[2];
+
+// Use Bracket Notation to Find the Last Character in a String
+// 37 Use bracket notation to find the last character in the lastName variable.
+
+// const lastName = "Lovelace";
+// const lastLetterOfLastName = lastName[lastName.length - 1];
+
+// Use Bracket Notation to Find the Nth-to-Last Character in a String
+// 38 Use bracket notation to find the second-to-last character in the lastName string.
+
+// const lastName = "Lovelace";
+// const secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
+// Word Blanks
+// 39 Word Blanks
+
+// const myNoun = "dog";
+// const myAdjective = "big";
+// const myVerb = "ran";
+// const myAdverb = "quickly";
+// const wordBlanks = myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
+// console.log(wordBlanks);
+
+// Store Multiple Values in one Variable using JavaScript Arrays
+// 40 Modify the new array myArray so that it contains both a string and a number (in that order).
+
+// const myArray = ["umair", 22];
+
+// Nest one Array within Another Array
+// 41 Create a nested array called myArray.
+
+// const myArray = [["Umair",23],["uzair",23]];
+
+// Access Array Data with Indexes
+// 42 Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+
+// const myArray = [50, 60, 70];
+// const myData = myArray[0];
+
+// Modify Array Data With Indexes
+// 43 Modify the data stored at index 0 of myArray to a value of 45.
+
+// const myArray = [18, 64, 99];
+// myArray[0] = 45; // this is called mutalbe
+// console.log(myArray);
+
+// Access Multi-Dimensional Arrays With Indexes
+// 44 Using bracket notation select an element from myArray such that myData is equal to 8.
+
+// const myArray = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [[10, 11, 12], 13, 14],
+//   ];
+
+//   const myData = myArray[2][1];
+
+// Manipulate Arrays With push Method
+// 45 Push ["dog", 3] onto the end of the myArray variable.
+
+// const myArray = [
+//   ["John", 23],
+//   ["cat", 2],
+// ];
+// myArray.push(["dog", 3]);
+
+// Manipulate Arrays With pop Method
+// 46 Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
+
+// const myArray = [["John", 23], ["cat", 2]];
+// const removedFromMyArray = myArray.pop()
+
+// Manipulate Arrays With shift Method
+// 46 Use the .shift() function to remove the first item from myArray and assign the "shifted off" value to a new variable, removedFromMyArray.
+
+// const myArray = [
+//   ["John", 23],
+//   ["dog", 3],
+// ];
+// const removedFromMyArray = myArray.shift();
+
+// Manipulate Arrays With unshift Method
+// 47 Add ["Paul", 35] to the beginning of the myArray variable using unshift().
+
+// const myArray = [["John", 23], ["dog", 3]];
+// myArray.shift();
+// myArray.unshift(["Paul", 35])
+
+// const myList = [];
+// myList.unshift(["laptops", 10], ["phones", 20], ["lcd", 30], ["tablet", 40], ["ipad", 50]);
+// console.log(myList);
